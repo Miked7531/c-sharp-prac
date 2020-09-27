41fde49ef0fbce9fa08adbeb6bc4b9d436e275d1
Mike <miked7531@gmail.com>
2020-09-26 22:43:47 -0400

    simple object
    basic understanding of the inner workings of objects in c#

---
+++ ObjectLifetime/ObjectLifetime/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectLifetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+
+            Car.MyMethod();
+
+            /*
+            myCar.Make = "Oldsmobile";
+            myCar.Model = "Cutlas Supreme";
+            myCar.Year = 1986;
+            myCar.Color = "Silver";
+            */
+
+            //Car myThirdCar = new Car("Ford", "Escape", 2005, "White");
+
+            /*
+            Car myOtherCar;
+            myOtherCar = myCar;
+
+            Console.WriteLine("{0} {1} {2} {3}",
+                myOtherCar.Make,
+                myOtherCar.Model,
+                myOtherCar.Year,
+                myOtherCar.Color);
+
+            myOtherCar.Model = "98";
+
+            Console.WriteLine("{0} {1} {2} {3}",
+                myCar.Make,
+                myCar.Model,
+                myCar.Year,
+                myCar.Color);
+
+            myOtherCar = null;
+
+            
+            Console.WriteLine("{0} {1} {2} {3}",
+                myOtherCar.Make,
+                myOtherCar.Model,
+                myOtherCar.Year,
+                myOtherCar.Color);
+            
+            myCar = null;
+            */
+
+            Console.ReadLine();
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        /*
+        public Car()
+        {
+            // you could load from a configuration file,
+            // a database, etc.. to get an object into a valid state so its usuable.
+            Make = "Nissan";
+        }
+
+        public Car(string make, string model, int year, string color)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Color = color;
+        }
+        */
+
+        public static void MyMethod()
+        {
+            Console.WriteLine("Called the static MyMethod");
+        }
+
+    }
+}
