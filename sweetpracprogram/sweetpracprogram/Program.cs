df3413e2ceaef0fba3324a76faadd91b2332636e
Mike <miked7531@gmail.com>
2020-09-29 18:02:45 -0400

    loop prac

--- sweetpracprogram/sweetpracprogram/Program.cs
+++ sweetpracprogram/sweetpracprogram/Program.cs
@@ -6,12 +6,27 @@ namespace sweetpracprogram
     {
         static void Main(string[] args)
         {
+            for(int i = 9; i >= 0; i-=2)
+            {
+                Console.WriteLine(i);
+            }
+            /*
+            int i = 0;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (i < 0);
+            */
+            /*
             int i = 0; // initialization
             while (i < 10) // condition
             {
                 Console.WriteLine(i);
                 i++; //update
             }
+            */
         }
     }
 }
