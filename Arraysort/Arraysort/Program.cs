865c62a75efbf6f0ad98527debea19e6e17ca936
Mike <miked7531@gmail.com>
2020-10-03 16:16:32 -0400

    sorting Array

---
+++ Arraysort/Arraysort/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Arraysort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] grades1 = { 30, 430, 23, 10 };
+            int[] grades2 = { 30, 430, 23, 10 };
+
+            if(Enumerable.SequenceEqual(grades1,grades2))
+            {
+                Console.WriteLine("equal");
+            }
+            
+            
+    }
+    }
+}
