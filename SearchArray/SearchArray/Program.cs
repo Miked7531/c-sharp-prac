dcc81d302021dba9d2135414acce014b708d4e39
Mike <miked7531@gmail.com>
2020-10-03 15:50:50 -0400

    search arrayindex

---
+++ SearchArray/SearchArray/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SearchArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] grades = { 30, 430, 23, 10 };
+
+            Console.WriteLine(Array.IndexOf(grades, 23));
+        }
+    }
+}
