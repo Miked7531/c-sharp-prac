a0bfd560f9dcf15c7f8a538fc31e9b35a332d58e
Mike <miked7531@gmail.com>
2020-10-03 15:02:59 -0400

    array length user input

---
+++ ArrayPrac1/ArrayPrac1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayPrac1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = Convert.ToInt32(Console.In.ReadLine());
+            int[] grades = new int[size];
+            
+            for (int i = 0; i < grades.Length; i++)
+            {
+                grades[i] = Convert.ToInt32(Console.In.ReadLine());
+            }
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                Console.Write(grades[i] + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
