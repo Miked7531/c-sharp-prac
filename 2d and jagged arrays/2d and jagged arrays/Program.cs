85b897cbee816205d848839ab9f393d103ccd4a7
Mike <miked7531@gmail.com>
2020-10-03 16:33:15 -0400

    2D and jagged arrays

---
+++ 2d and jagged arrays/2d and jagged arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _2d_and_jagged_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] grades = {
+                {5,4,7,3 },
+                {7,3,2,6 },
+                {5,3,7,3 }
+            };
+            
+            Console.WriteLine(grades.GetLength(0));
+            Console.WriteLine(grades.GetLength(1));
+            
+            for (int i = 0; i < grades.GetLength(0); i++)
+            {
+                for (int k = 0; k < grades.GetLength(1); k++)
+                {
+                    Console.Write(grades[i,k] + " ");
+                }
+                Console.WriteLine();
+            }
+            
+        }   
+    }
+}
