eaeb7420e32476e1b6b8644bd6f8e420ac1e7acc
Mike <miked7531@gmail.com>
2021-05-14 17:12:34 -0400

    prac fun stuff

---
+++ PoopFish/PoopFish/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Runtime.InteropServices;
+
+
+namespace PoopFish
+{
+    class Program
+    {
+        public const UInt32 WM_KEYDOWN;
+        public const UInt32 WM_KEYUP;
+
+        public enum keyState
+        {
+            KEYDOWN;
+            KEYUP;
+        }
+
+
+    }
+}
---
+++ ReadFromFileIO/ReadFromFileIO/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ReadFromFileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string text = File.ReadAllText(@"C:\Users\miked\Desktop\poo\poo.txt");
+
+            Console.WriteLine("Textfile contains the following: {0}", text);
+
+            string[] lines = File.ReadAllLines(@"C:\Users\miked\Desktop\poo\poo.txt");
+
+            Console.WriteLine("Contents of poo.txt = ");
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\t" + line);
+            }
+
+            Console.ReadKey();
+            */
+        }
+    }
+}
